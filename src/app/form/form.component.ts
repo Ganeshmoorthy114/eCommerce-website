7fdde92677502afe7f191463af7c67ca20874237
Ganeshmoorthy114 <ganeshmoorthy17206734@gmail.com>
2022-12-26 15:15:05 +0530

    first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  
+  data:FormGroup;
+  studentData: any=[];
+  constructor(private fb:FormBuilder) {
+    this.data= this.fb.group({
+      name: ["", Validators.required],
+      Lastname: ["", Validators.required],
+      Email: ["", Validators.required],
+      Password: ["", Validators.required]
+    })
+   }
 
-  constructor() { }
-
+   get add() {
+    return this.data.controls
+  }
   ngOnInit() {
+    let parse=JSON.parse(localStorage.getItem("list"))
+    
+    if(parse!=null){
+      this.studentData=parse
+debugger
+    }
+  }
+  submit(){
+    var getForm=this.data.value
+    if(this.data.invalid) {
+      this.data.markAllAsTouched()
+      
+    } 
+    {
+   
+      this.studentData.push(getForm)
+      let data=JSON.stringify(this.studentData)
+    
+      localStorage.setItem("list",data)
+      this.data.reset()
+  }
   }
 
+  removeData(i){
+    this.studentData.splice(i,1)
+    var data=JSON.stringify(this.studentData)
+    localStorage.setItem("list",data)
+  }
 }
